1da0a2564a5c6b8b4670401aa42d76074f71a714
Zhibin Huang <benjaminhuang1@gmail.com>
2013-11-10 17:54:29 -0500

    Module test

--- step1/js/main.js
+++ step1/js/main.js
@@ -14,40 +14,58 @@ require.config({
 
 require( ['sprite','jquery'], function(Sprite3D, $ ){
 	console.log( Sprite3D );
-	var isSupported = Sprite3D.isSupported(), width = $('#stage').width();
-
-  console.log( isSupported );	
+	var isSupported = Sprite3D.isSupported();
+	var sta = { 
+			width: $('#stage').width(),
+			height: $('#stage').height()
+		};
+	var win = {
+			width: $(window).width(),
+			height: $(window).height()
+		};
+		
+ 	console.log( isSupported );	
 
 	console.log( document.querySelector("#stage") );
 	console.log( $('#stage').get(0) );
 
-	var stage = Sprite3D.stage($('#stage').get(0)).origin(width/2, 0, 0).position(width/2,0,0).rotation(5,0,0).perspective(400).update();
-
-	// hack 
+	var stage = Sprite3D.stage($('#stage').get(0)).origin(0, 0, 0).position(0 ,0,0).perspective(400).update();
 	$('#stage').css(Sprite3D._browserPrefix+'TransformStyle', 'preserve-3d');
 
-	var upper = Sprite3D.create('#upper').position(0,-150, 50).update(),
-		lower = Sprite3D.create('#lower').position(0,400,200).update(),
-		object = Sprite3D.create('#object').position(width/2-30,290,180).update(),
-		bg = Sprite3D.create('#bg').position(-width*2,200,-200).update();
+	var container = Sprite3D.create('#container').origin(0,0,0).position(0,0,0).update();
 
-	stage.appendChild( bg );
-	stage.appendChild( upper );
-	stage.appendChild( lower );
-	stage.appendChild( object );
+	console.log(stage);
+	var bg = Sprite3D.create('#bg').transformOrigin(0,0).position(-sta.width*2,0,-1000).update();
+	var object = Sprite3D.create('.object obj').position(300,100,100).origin(0,0,0).update();
 
+	stage.appendChild( container );
+	container.appendChild( bg );
+	container.appendChild( object );
+
+console.log($('#bg'));
+	$('#bg').css('top','500px');
 
 	$(document).resize(function(){
-		width = $('#stage').width();
+		win = {
+			width: $(window).width(),
+			height: $(window).height()
+		};
 	});
 
 	$(document).mousemove(function(e){
-		var dt = e.pageX - width/2, dg = 16;
-		console.log(dt);
-		stage.origin((e.pageX-width/2)*0.2+width/2,-50,0).rotation(5+Math.abs(e.pageX-width/2)*0.005, 0, 0).update();
-		bg.rotation(0 ,dt/dg, 0).update();
-		upper.rotation(0 ,dt/dg, 0).update();
-		lower.rotation(0 ,dt/(dg*2), 0).update();
-		object.rotation(0 ,dt/(dg*1.5), 0).update();
+
+		var dx = e.pageX - win.width/2;
+		var dy = e.pageY - win.height/2;
+
+		var dg = 8;
+
+		//console.log(dx);
+		//container.origin((e.pageX-width/2)*0.2+width/2,-200,0).rotation(Math.abs(e.pageX-width/2)*0.00, 0, 0).update();
+		//stage.rotation(dy/dg, dx/dg, 0).update();
+		
+		container.rotation(dy/dg, dx/dg, 0).update();
+		bg.rotation(dy/dg ,dx/dg, 0).update();
+		//object.rotation(0 ,dx/(dg), 0).update();
+
 	});
 });
